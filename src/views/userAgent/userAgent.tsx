8d90cfcdc2a74d58204eac624b8e45282b890bb9
Mehedi hasan <162013416+Meehedi-hasan@users.noreply.github.com>
2024-12-24 00:35:56 +0600

    first commit

---
+++ src/views/userAgent/userAgent.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { BackToHome } from "@/components/backToHome/backToHome";
+import { useUserAgentContext } from "@/components/providers/userAgentProvider";
+
+export const UserAgent = () => {
+  const { userAgent } = useUserAgentContext();
+
+
+  return (
+    <div>
+      <BackToHome />
+      {userAgent ? (
+        <div className="flex font-mono font-semibold text-sm">
+          <div className="border p-2">UserAgent</div>
+          <div className="border p-2">{userAgent}</div>
+        </div>
+      ) : (
+        <div className="flex font-mono font-semibold text-sm">
+          <div className="border p-2">UserAgent</div>
+          <div className="border p-2">Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36</div>
+        </div>
+      )}
+    </div>
+  );
+};
